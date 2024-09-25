e6069affa87862064c46d5db78cc746cb735680b
otacleT <mytspe46@gmail.com>
2024-09-25 13:47:50 +0900

    feat: add final file

--- final/draw-model.ts
+++ final/draw-model.ts
@@ -22,13 +22,26 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
 // 光源を追加
-// const light = new THREE.DirectionalLight("#ffffff", 2);
-// const ambient = new THREE.AmbientLight("#85b2cd");
-// light.position.set(10, 10, 0).normalize();
-// scene.add(light);
-// scene.add(ambient);
+const light = new THREE.DirectionalLight("#ffffff", 2);
+const ambient = new THREE.AmbientLight("#85b2cd");
+light.position.set(10, 10, 0).normalize();
+scene.add(light);
+scene.add(ambient);
 
 // 4. 3Dオブジェクトを定義、sceneに追加
+let model: THREE.Group<THREE.Object3DEventMap>;
+const loader = new GLTFLoader();
+loader.load(
+	"../public/apple.glb",
+	(gltf: GLTF) => {
+		model = gltf.scene;
+		scene.add(gltf.scene);
+	},
+	undefined,
+	() => {
+		console.log("読み込みに失敗しました 😢");
+	},
+);
 
 // Controller
 // const controls = new OrbitControls(camera, renderer.domElement);
@@ -38,6 +51,9 @@ function animate() {
 	requestAnimationFrame(animate);
 
 	// controls.update();
+
+	// りんごを水平方向へ回転するアニメーションを追加 ( 60fps )
+	if (model) model.rotation.y += 0.01;
 
 	renderer.render(scene, camera);
 }
